00fd4f797d7c5f79b37b8b400b6d81a8feebf22f
ssarun9871 <ssarun9871@gmail.com>
2022-12-23 12:54:01 +0530

    adding delete button

--- models/cart.js
+++ models/cart.js
@@ -37,4 +37,20 @@ module.exports = class Cart {
       });
     });
   }
+
+  static deleteProduct(id,productPrice){
+    fs.readFile(p,(err,fileContent)=>{
+      if(err){return;}
+      const updatedCart = {...JSON.stringify(fileContent)};
+      const product = updatedCart.products.find(prod=> prod.id===id);
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(prod=>prod.id!==id);
+      updatedCart.totalPrice = updatedCart.totalPrice-productPrice*productQty;
+
+      fs.writeFile(p,JSON.stringyfy(updatedCart),err=>{
+        console.log(err);
+      })
+
+    })
+  }
 };
